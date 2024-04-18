f6a0f189e1411cb7cdc4ce8a4b4078aed6544dc8
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-04-18 01:02:27 -0400

    Update list of words

--- words.js
+++ words.js
@@ -2,4 +2,13 @@ export const WORDS = [
   ['chair', 'стул'],
   ['sofa', 'диван'],
   ['table', 'стол'],
+  ['lamp', 'лампа'],
+  ['female_student', 'студентка'],
+  ['male_student', 'студент'],
+  ['fork', 'вилка'],
+  ['pen', 'ручка'],
+  ['pencil', 'карандаш'],
+  ['rose', 'роза'],
+  ['to_read', 'читать'],
+  ['to_write', 'писать'],
 ]
